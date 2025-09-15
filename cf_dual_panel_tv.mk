8049cee8b504f7905aa65782d0e8f45cdd763076
DonizeteVida <donizetevida1998@outlook.com>
2025-09-16 00:52:29 +0200

    Life is short

--- cf_dual_panel_tv.mk
+++ cf_dual_panel_tv.mk
@@ -1,7 +1,7 @@
 #
 # All components inherited here go to system image
 #
-$(call inherit-product, device/google/atv/products/atv_system.mk)
+$(call inherit-product, device/google/atv/products/atv_generic_system.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -16,8 +16,11 @@ $(call inherit-product, device/google/atv/products/atv_product.mk)
 #
 # All components inherited here go to vendor image
 #
-$(call inherit-product, device/google/atv/products/atv_vendor.mk)
+$(call inherit-product, device/google/atv/products/atv_emulator_vendor.mk)
 
-PRODUCT_NAME := Cuttlefish Dual Panel TV
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS = false
+
+PRODUCT_NAME := cuttlefish_dual_panel_tv
 PRODUCT_DEVICE := cf_dual_panel_tv
 PRODUCT_BRAND := DonizeteCompany
+PRODUCT_MODEL := Cuttlefish Dual Panel TV
